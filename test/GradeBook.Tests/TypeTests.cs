159bbf1feb54e77f6cc2935222f8e92b42bfa748
Rami Elsayed <ramielsayed123@outlook.com>
2022-10-16 18:23:21 +0100

    tested different types of data in C#

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -11,6 +11,50 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void Test1()
+        {
+            var x = GetInt();
+            SetInt(x);
+
+            Assert.Equal(3, x);
+        }
+
+        private void SetInt(int z)
+        {
+            z = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpIsPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Book");
+            Assert.Equal("New Book", book1.Name);
+        }
+
+        private void GetBookSetName(ref Book book, string name)
+        {
+            book = new Book(name);
+        }
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Book");
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
         public void CanSetNameFromReference()
         {
             var book1 = GetBook("Book 1");
@@ -22,6 +66,21 @@ namespace GradeBook.Tests
         private void SetName(string name, Book book)
         {
              book.Name = name;
+        }
+
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Rami";
+            var upper = MakeUpperCase(name);
+
+            Assert.Equal("Rami", name);
+            Assert.Equal("RAMI", upper);
+        }
+
+        private object MakeUpperCase(string name)
+        {
+            return name.ToUpper();
         }
 
         [Fact]
