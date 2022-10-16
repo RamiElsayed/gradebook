d16f10d70575b80a7669b31a78172fdd1ff032b4
Rami Elsayed <ramielsayed123@outlook.com>
2022-10-16 21:05:27 +0100

    buiilt some validation with try catch for exceptions

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -19,7 +19,28 @@ namespace GradeBook
             }
             else
             {
-                Console.WriteLine("Invalid Grade");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
+        }
+
+        public void AddLetterGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                default:
+                    break;
             }
         }
 
@@ -38,6 +59,28 @@ namespace GradeBook
                 result.Average += grades[i];
             }
             result.Average /= grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                case var d when d >= 50.0:
+                    result.Letter = 'E';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
